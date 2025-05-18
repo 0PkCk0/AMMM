101a21348f5d2fb979afc1ab12f019ecd1442d66
0PkCk0 <patrick.cerka@estudiantat.upc.edu>
2025-05-18 18:19:18 +0200

    code factor and corrector on local search

--- greedy_lc.cpp
+++ greedy_lc.cpp
@@ -5,8 +5,7 @@
 
 using namespace std;
 
-vector<vector<int>> only_greedy(vector<vector<int>> m, int N) {
-  vector<vector<int>> x(N, vector<int>(N, 0));
+vector<vector<int>> corrector(vector<vector<int>> x, const vector<vector<int>>& m, int N) {
   vector<float> averages(N, 0);
 
   for (int i = 0; i < N; ++i) {
@@ -16,12 +15,33 @@ vector<vector<int>> only_greedy(vector<vector<int>> m, int N) {
     }
     averages[i] = sum / N;
   }
+
   int max_i = 0;
-  for (int i = 0; i < N; i++) {
+  for (int i = 1; i < N; ++i) {
     if (averages[i] > averages[max_i]) {
       max_i = i;
     }
   }
+
+  for (int i = 0; i < N; ++i) {
+    if (x[i][max_i] != 0) {
+      x[i][max_i] = 0;
+      int max_loc = 0;
+      for (int j = 0; j < N; ++j) {
+        if (x[max_i][j] == 0 && m[max_i][j] > m[max_i][max_loc] && j != max_i) {
+          max_loc = j;
+        }
+      }
+      x[max_i][max_loc] = 1;
+    }
+  }
+
+  return x;
+}
+
+
+vector<vector<int>> only_greedy(vector<vector<int>> m, int N) {
+  vector<vector<int>> x(N, vector<int>(N, 0));
 
   for (int i = 0; i < N; i++) {
     for (int j = 0; j < N; j++) {
@@ -35,63 +55,53 @@ vector<vector<int>> only_greedy(vector<vector<int>> m, int N) {
     }
   }
 
-  for (int i = 0; i < N; i++) {
-    if (x[i][max_i] != 0) {
-      x[i][max_i] = 0;
-	  int max_loc=0;
-      for (int j=0; j<N; j++) {
-		if(x[max_i][j]==0 && m[max_i][j] > m[max_i][max_loc] && j != max_i){
-			max_loc = j;
-		}
-	  }
-	  x[max_i][max_loc] = 1;
-    }
-  }
+  
 
-  return x;
+  return corrector(x, m, N);
 }
 
-int evaluate(const vector<vector<int>>& x, const vector<vector<int>>& m, int N) {
+int evaluate(const vector<vector<int>> &x, const vector<vector<int>> &m,
+             int N) {
   int score = 0;
 
   for (int i = 0; i < N; ++i) {
-      for (int j = 0; j < N; ++j) {
-          if (x[i][j] != 0) {
-              score += m[i][j];
-          }
+    for (int j = 0; j < N; ++j) {
+      if (x[i][j] != 0) {
+        score += m[i][j];
       }
+    }
   }
 
   return score;
 }
 
-
-vector<vector<int>> local_search(vector<vector<int>> x, vector<vector<int>> m, int N) {
+vector<vector<int>> local_search(vector<vector<int>> x, vector<vector<int>> m,
+                                 int N) {
   int best_score = evaluate(x, m, N);
   bool improved = true;
 
   while (improved) {
-      improved = false;
-      for (int i = 0; i < N; ++i) {
-          for (int j = i + 1; j < N; ++j) {
-              if (x[i][j] != x[j][i]) {
-                  // Try flipping
-                  swap(x[i][j], x[j][i]);
-                  int new_score = evaluate(x, m, N);
-                  if (new_score > best_score) {
-                      best_score = new_score;
-                      improved = true;
-                  } else {
-                      // Revert if no improvement
-                      swap(x[i][j], x[j][i]);
-                  }
-              }
+    improved = false;
+    for (int i = 0; i < N; ++i) {
+      for (int j = i + 1; j < N; ++j) {
+        if (x[i][j] != x[j][i]) {
+          // Try flipping
+          swap(x[i][j], x[j][i]);
+          int new_score = evaluate(x, m, N);
+          if (new_score > best_score) {
+            best_score = new_score;
+            improved = true;
+          } else {
+            // Revert if no improvement
+            swap(x[i][j], x[j][i]);
           }
+        }
       }
+    }
   }
-  return x;
-}
 
+  return corrector(x, m, N);
+}
 
 int main() {
   int N = 3;
