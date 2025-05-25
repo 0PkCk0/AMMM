aa01456caa2a118d89770bdfcaa3a98f4c2556ec
0PkCk0 <patrick.cerka@estudiantat.upc.edu>
2025-05-25 16:12:37 +0200

    new correct version

--- greedy_lc.cpp
+++ greedy_lc.cpp
@@ -1,183 +1,178 @@
 #include "iostream"
-#include <cstdio>
-#include <ostream>
-#include <vector>
 #include <algorithm>
 #include <cmath>
+#include <cstdio>
 #include <cstdlib>
 #include <ctime>
 #include <fstream>
+#include <ostream>
 #include <sstream>
+#include <vector>
 
 using namespace std;
 
-vector<vector<int>> corrector(vector<vector<int>> x, const vector<vector<int>> m, int N)
-{
-  vector<float> averages(N, 0);
-
-  for (int i = 0; i < N; ++i)
-  {
-    float sum = 0;
-    for (int j = 0; j < N; ++j)
-    {
-      sum += m[i][j];
-    }
-    averages[i] = sum / N;
-  }
-
-  int max_i = 0;
-  for (int i = 1; i < N; ++i)
-  {
-    if (averages[i] > averages[max_i])
-    {
-      max_i = i;
-    }
-  }
-
-  for (int i = 0; i < N; ++i)
-  {
-    if (x[i][max_i] != 0)
-    {
-      x[i][max_i] = 0;
-      x[max_i][i] = 1;
-    }
-  }
-
-  return x;
-}
-
-struct grasp_struct
-{
-  int index;
-  int value;
-  grasp_struct(int i, int v) : index(i), value(v) {}
-  grasp_struct() : index(0), value(0) {}
-};
-bool comp(const grasp_struct &a, const grasp_struct &b)
-{
-  return a.value > b.value;
-}
-
-vector<vector<int>> grasp_greedy(const vector<vector<int>> m, int N, float alpha)
-{
-  vector<grasp_struct> averages(N, grasp_struct(0, 0));
-  vector<vector<int>> x(N, vector<int>(N, 0));
-
-  for (int i = 0; i < N; ++i)
-  {
-    float sum = 0;
-    for (int j = 0; j < N; ++j)
-    {
-      sum += m[i][j];
-    }
-    averages[i].value = sum / N;
-    averages[i].index = i;
-  }
-
-  sort(averages.begin(), averages.end(), comp);
-  averages.resize(N * alpha+1);
-  
-  srand(time(0));
-  for (int i = 0; i < N; i++)
-  {
-    int random_index = rand() % N * alpha;
-    int struct_index = 0;
-    for (int j = 0; j < N * alpha; j++)
-    {
-      if (averages[j].index == random_index)
-      {
-        struct_index = j;
-        break;
-      }
-    }
-    x[struct_index][i] = 1;
-  }
-
-  return corrector(x, m, N);
-}
-
-vector<vector<int>> only_greedy(vector<vector<int>> m, int N)
-{
-  vector<vector<int>> x(N, vector<int>(N, 0));
-
-  for (int i = 0; i < N; i++)
-  {
-    for (int j = 0; j < N; j++)
-    {
-      if (i != j && x[i][j] == 0 && x[j][i] == 0)
-      {
-        if (m[i][j] > m[j][i])
-        {
-          x[i][j] = 1;
-        }
-        else
-        {
-          x[j][i] = 1;
-        }
-      }
-    }
-  }
-
-  return corrector(x, m, N);
-}
-
-int evaluate(const vector<vector<int>> &x, const vector<vector<int>> &m,
-             int N)
-{
+int evaluate_order(const vector<int>& order, const vector<vector<int>>& m) {
+  int N = order.size();
   int score = 0;
-
-  for (int i = 0; i < N; ++i)
-  {
-    for (int j = 0; j < N; ++j)
-    {
-      if (x[i][j] != 0)
-      {
-        score += m[i][j];
+  // For every pair i < j, add m[order[i]][order[j]]
+  for (int i = 0; i < N; ++i) {
+      for (int j = i + 1; j < N; ++j) {
+          score += m[order[i]][order[j]];
       }
-    }
   }
-
   return score;
 }
 
-vector<vector<int>> local_search(vector<vector<int>> x, vector<vector<int>> m,
-                                 int N)
-{
-  int best_score = evaluate(x, m, N);
+void local_search_order(vector<int>& order, const vector<vector<int>>& m) {
+  int N = order.size();
   bool improved = true;
 
-  while (improved)
-  {
-    improved = false;
-    for (int i = 0; i < N; ++i)
-    {
-      for (int j = i + 1; j < N; ++j)
-      {
-        if (x[i][j] != x[j][i])
-        {
-          // Try flipping
-          swap(x[i][j], x[j][i]);
-          int new_score = evaluate(x, m, N);
-          if (new_score > best_score)
-          {
-            best_score = new_score;
-            improved = true;
-          }
-          else
-          {
-            // Revert if no improvement
-            swap(x[i][j], x[j][i]);
-          }
-        }
-      }
-    }
-  }
+  while (improved) {
+      improved = false;
+      // Try swapping adjacent pairs
+      for (int i = 0; i < N - 1; ++i) {
+          swap(order[i], order[i + 1]);
+          int new_score = evaluate_order(order, m);
+          swap(order[i], order[i + 1]); // revert
 
-  return corrector(x, m, N);
+          int current_score = evaluate_order(order, m);
+
+          if (new_score > current_score) {
+              // Accept swap
+              swap(order[i], order[i + 1]);
+              improved = true;
+          }
+      }
+  }
 }
 
-int main()
-{
+void buildAcyclicPriority(const vector<vector<int>> &m, int N) {
+  
+  vector<pair<int,int>> node_score; 
+  for (int i = 0; i < N; ++i) {
+      int sum_row = 0;
+      for (int j = 0; j < N; ++j) {
+          sum_row += m[i][j];
+      }
+      node_score.emplace_back(sum_row, i);
+  }
+
+  
+  sort(node_score.rbegin(), node_score.rend());
+
+  
+  vector<int> order(N);
+  for (int i = 0; i < N; ++i) {
+      order[i] = node_score[i].second;
+  }
+
+  vector<vector<int>> x(N, vector<int>(N, 0));
+  for (int i = 0; i < N; ++i) {
+      for (int j = i + 1; j < N; ++j) {
+          int u = order[i];
+          int v = order[j];
+          x[u][v] = 1;  
+      }
+  }
+  
+  int counter = 0;
+  for (int i = 0; i < N; ++i) {
+    for (int j = 0; j < N; ++j) {
+      if (x[i][j] == 1)
+        counter += m[i][j];
+      cout << x[i][j] << " ";
+    }
+    cout << endl;
+  }
+
+  cout << "Points with only greedy: " << counter << endl;
+  counter = 0;
+  cout << "The greedy + local search Solution:" << endl;
+  local_search_order(order, m);
+
+  vector<vector<int>> y(N, vector<int>(N, 0));
+  for (int i = 0; i < N; ++i) {
+      for (int j = i + 1; j < N; ++j) {
+          int u = order[i];
+          int v = order[j];
+          y[u][v] = 1;  
+      }
+  }
+
+  for (int i = 0; i < N; ++i) {
+    for (int j = 0; j < N; ++j) {
+      if (y[i][j] == 1)
+        counter += m[i][j];
+      cout << y[i][j] << " ";
+    }
+    cout << endl;
+  }
+
+  cout << "Points with greedy solution and local search enhancement: "<< counter << endl;
+}
+
+void grasp(const vector<vector<int>>& m, float alpha) {
+  int N = m.size();
+  vector<int> order;
+  vector<bool> chosen(N, false);
+
+  
+  vector<int> scores(N, 0);
+  for (int i = 0; i < N; ++i) {
+      int s = 0;
+      for (int j = 0; j < N; ++j) {
+          s += m[i][j];
+      }
+      scores[i] = s;
+  }
+
+  srand(time(nullptr));
+
+  while ((int)order.size() < N) {
+      
+      vector<pair<int, int>> candidates;
+      for (int i = 0; i < N; ++i) {
+          if (!chosen[i]) {
+              candidates.push_back({scores[i], i});
+          }
+      }
+
+      
+      sort(candidates.rbegin(), candidates.rend());
+
+      
+      int rcl_size = max(1, int(alpha * candidates.size()));
+
+      
+      int pick = rand() % rcl_size;
+
+      int selected = candidates[pick].second;
+      order.push_back(selected);
+      chosen[selected] = true;
+  }
+
+  vector<vector<int>> y(N, vector<int>(N, 0));
+  for (int i = 0; i < N; ++i) {
+      for (int j = i + 1; j < N; ++j) {
+          int u = order[i];
+          int v = order[j];
+          y[u][v] = 1;  
+      }
+  }
+
+  int counter = 0;
+  for (int i = 0; i < N; ++i) {
+    for (int j = 0; j < N; ++j) {
+      if (y[i][j] == 1)
+        counter += m[i][j];
+      cout << y[i][j] << " ";
+    }
+    cout << endl;
+  }
+  cout << "Points with GRASP solution: " << counter << endl;
+}
+
+int main() {
   int N;
   vector<vector<int>> m;
 
@@ -185,13 +180,10 @@ int main()
   string line;
 
   // Read N
-  while (getline(infile, line))
-  {
-    if (line.find("N") != string::npos) 
-    {
+  while (getline(infile, line)) {
+    if (line.find("N") != string::npos) {
       size_t eq = line.find('=');
-      if (eq != string::npos)
-      {
+      if (eq != string::npos) {
         N = stoi(line.substr(eq + 1));
       }
       break;
@@ -199,24 +191,20 @@ int main()
   }
 
   // Skip to matrix
-  while (getline(infile, line))
-  {
+  while (getline(infile, line)) {
     if (line.find('[') != string::npos)
       break;
   }
 
   // Read matrix rows
-  for (int i = 0; i < N; ++i)
-  {
+  for (int i = 0; i < N; ++i) {
     getline(infile, line);
     vector<int> row;
     stringstream ss(line);
     char ch;
     int val;
-    while (ss >> ch)
-    {
-      if (isdigit(ch) || ch == '-')
-      {
+    while (ss >> ch) {
+      if (isdigit(ch) || ch == '-') {
         ss.putback(ch);
         ss >> val;
         row.push_back(val);
@@ -226,57 +214,16 @@ int main()
   }
 
   cout << "Matrix m:" << endl;
-  for (int i = 0; i < N; ++i)
-  {
-    for (int j = 0; j < N; ++j)
-    {
+  for (int i = 0; i < N; ++i) {
+    for (int j = 0; j < N; ++j) {
       cout << m[i][j] << " ";
     }
     cout << endl;
   }
 
   cout << "The only greedy Solution:" << endl;
-  vector<vector<int>> x = only_greedy(m, N);
-  int counter = 0;
-  for (int i = 0; i < N; ++i)
-  {
-    for (int j = 0; j < N; ++j)
-    {
-      if (x[i][j] != 0) counter+=m[i][j];
-      cout << x[i][j] << " ";
-    }
-    cout << endl;
-  }
-
-  cout << "Points with only greedy: " << counter << endl;
-  counter = 0;
-  cout << "The greedy + local search Solution:" << endl;
-  vector<vector<int>> y = local_search(x, m, N);
-  for (int i = 0; i < N; ++i)
-  {
-    for (int j = 0; j < N; ++j)
-    {
-      if (y[i][j] != 0) counter+=m[i][j];
-      cout << y[i][j] << " ";
-    }
-    cout << endl;
-  }
-
-  cout << "Points with greedy solution and local search enhancement: " << counter << endl;
-  counter = 0;
-  cout << "The grasp solution:" << endl;
-  vector<vector<int>> z = grasp_greedy(m, N, 0.4);
-  for (int i = 0; i < N; ++i)
-  {
-    for (int j = 0; j < N; ++j)
-    {
-      if (z[i][j] != 0) counter+=m[i][j];
-      cout << z[i][j] << " ";
-    }
-    cout << endl;
-  }
-
-  cout << "Points with GRASP solution: " << counter << endl;
-
+  buildAcyclicPriority(m, N);
+  
+  grasp(m, 0.1);
   return 0;
 }
